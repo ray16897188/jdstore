ca484980abce173d52554967d62ac2f0e3776bab
Ray Gao <atlantis.ray@gmail.com>
2016-11-04 23:49:17 +0800

    product detail 1 2 3 backup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,89 +20,24 @@
 
 $(document).ready(function() {
  
-  var time = 7; // time in seconds
+  $("#owl-demo").owlCarousel({
  
-  var $progressBar,
-      $bar, 
-      $elem, 
-      isPause, 
-      tick,
-      percentTime;
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
  
-    //Init the carousel
-    $("#owl-demo").owlCarousel({
-      slideSpeed : 500,
-      paginationSpeed : 500,
-      singleItem : true,
-      afterInit : progressBar,
-      afterMove : moved,
-      startDragging : pauseOnDragging
-    });
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
  
-    //Init progressBar where elem is $("#owl-demo")
-    function progressBar(elem){
-      $elem = elem;
-      //build progress bar elements
-      buildProgressBar();
-      //start counting
-      start();
-    }
- 
-    //create div#progressBar and div#bar then prepend to $("#owl-demo")
-    function buildProgressBar(){
-      $progressBar = $("<div>",{
-        id:"progressBar"
-      });
-      $bar = $("<div>",{
-        id:"bar"
-      });
-      $progressBar.append($bar).prependTo($elem);
-    }
- 
-    function start() {
-      //reset timer
-      percentTime = 0;
-      isPause = false;
-      //run interval every 0.01 second
-      tick = setInterval(interval, 10);
-    };
- 
-    function interval() {
-      if(isPause === false){
-        percentTime += 1 / time;
-        $bar.css({
-           width: percentTime+"%"
-         });
-        //if percentTime is equal or greater than 100
-        if(percentTime >= 100){
-          //slide to next item 
-          $elem.trigger('owl.next')
-        }
-      }
-    }
- 
-    //pause while dragging 
-    function pauseOnDragging(){
-      isPause = true;
-    }
- 
-    //moved callback
-    function moved(){
-      //clear interval
-      clearTimeout(tick);
-      //start again
-      start();
-    }
- 
-    //uncomment this to make pause on mouseover 
-    // $elem.on('mouseover',function(){
-    //   isPause = true;
-    // })
-    // $elem.on('mouseout',function(){
-    //   isPause = false;
-    // })
+  });
  
 });
+
 
 /* Modernizr 2.8.1 (Custom Build) | MIT & BSD
  * Build: http://modernizr.com/download/#-fontface-backgroundsize-borderimage-borderradius-boxshadow-flexbox-flexboxlegacy-hsla-multiplebgs-opacity-rgba-textshadow-cssanimations-csscolumns-generatedcontent-cssgradients-cssreflections-csstransforms-csstransforms3d-csstransitions-applicationcache-canvas-canvastext-draganddrop-hashchange-history-audio-video-indexeddb-input-inputtypes-localstorage-postmessage-sessionstorage-websockets-websqldatabase-webworkers-geolocation-inlinesvg-smil-svg-svgclippaths-touch-webgl-shiv-cssclasses-teststyles-testprop-testallprops-hasevent-prefixes-domprefixes-load
