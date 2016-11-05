eced2edd3d64c3d3fef25b5738164dc1183f7366
cndaniel <tigerljt@gmail.com>
2016-11-05 21:58:34 +0800

    21:58 5.11

--- app/uploaders/photos_uploader.rb
+++ app/uploaders/photos_uploader.rb
@@ -16,6 +16,10 @@ class PhotosUploader < CarrierWave::Uploader::Base
 
   process resize_to_fit: [800, 800]
 
+  version :xsthumb do
+    process resize_to_fill: [136, 136]
+  end
+
   version :thumb do
     process resize_to_fill: [200, 200]
   end
