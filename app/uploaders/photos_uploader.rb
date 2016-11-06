1592c0f59e61d6f93463b3691da33f5b3914ab45
cndaniel <tigerljt@gmail.com>
2016-11-07 06:06:45 +0800

    fix product show photos layout

--- app/uploaders/photos_uploader.rb
+++ app/uploaders/photos_uploader.rb
@@ -5,8 +5,8 @@ class PhotosUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-   #storage :file
-  storage :fog
+   storage :file
+  #storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -18,6 +18,10 @@ class PhotosUploader < CarrierWave::Uploader::Base
 
   version :xsthumb do
     process resize_to_fill: [170, 170]
+  end
+
+  version :prothumb do
+    process resize_to_fill: [145, 145]
   end
 
   version :thumb do
