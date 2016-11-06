f0dce9bb697bdc744cd694981a094c79145a7d47
cndaniel <tigerljt@gmail.com>
2016-11-06 22:15:20 +0800

    api adjustment

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,21 +5,21 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.storage :fog                           
+    config.storage :fog
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],      # 你的 key
+      aws_access_key_id:     ENV["S3_key_id"],      # 你的 key
 
 
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],      # 你的 secret key
+      aws_secret_access_key: ENV["S3_key_secret"],      # 你的 secret key
 
 
-      region:                'us-east-1' # 你的 S3 bucket 的 Region 位置
+      region:                'eu-central-1' # 你的 S3 bucket 的 Region 位置
 
 
     }
-    config.fog_directory  = ENV["AWS_BUCKET_NAME"] # 你设定的 bucket name 
-    
+    config.fog_directory  = 'creativestore-bucket' # 你设定的 bucket name
+
 
   else
    config.storage :file
